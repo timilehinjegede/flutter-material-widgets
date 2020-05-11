e1063c5c61ca85dcb078a4027723f48a3bc61a78
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-05-11 05:54:17 +0100

    Added the enum class for selecting group alignment

--- lib/home.dart
+++ lib/home.dart
@@ -5,6 +5,24 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
+enum GroupStatus { negativeOne, zero, positiveOne }
+
+double getGroupValue(GroupStatus groupStatus) {
+  switch (groupStatus) {
+    case GroupStatus.negativeOne:
+      return -1.0;
+      break;
+    case GroupStatus.zero:
+      return 0.0;
+      break;
+    case GroupStatus.positiveOne:
+      return 1.0;
+      break;
+    default:
+      return -2;
+  }
+}
+
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
 
